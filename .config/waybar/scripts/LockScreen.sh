76f30c438c9ecb241241137a9b7d46a8cea61ee1
Seb Day <sebday@gmail.com>
2025-06-18 21:34:32 +0100

    set main monitor for gtklock

--- .config/waybar/scripts/LockScreen.sh
+++ .config/waybar/scripts/LockScreen.sh
@@ -3,7 +3,7 @@ IDLE_TIMEOUT=900000  # 15 minutes in milliseconds
 while true; do
     IDLE_TIME=$(hyprctl -j activewindow | jq '.at[0] | .idle')
     if [ "$IDLE_TIME" -ge "$IDLE_TIMEOUT" ]; then
-        gtklock
+        gtklock --monitor-priority DP-2
     fi
     sleep 60
 done
