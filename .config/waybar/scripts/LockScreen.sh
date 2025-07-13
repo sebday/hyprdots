58b430ab679f27f9e71b1441d3bb1bf9b803ea9b
Seb Day <sebday@gmail.com>
2025-07-13 23:19:34 +0100

    hypridle

--- .config/waybar/scripts/LockScreen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IDLE_TIMEOUT=900000  # 15 minutes in milliseconds
-while true; do
-    IDLE_TIME=$(hyprctl -j activewindow | jq '.at[0] | .idle')
-    if [ "$IDLE_TIME" -ge "$IDLE_TIMEOUT" ]; then
-        gtklock --monitor-priority DP-2
-    fi
-    sleep 60
-done
