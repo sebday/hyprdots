b32f9939d85bc9f2bfbbcfb9b9c50fffcd9fdfeb
Seb Day <sebday@gmail.com>
2025-08-03 00:51:49 +0100

    gtk theme swap

--- .config/scripts/ThemeSwitcher.sh
+++ .config/scripts/ThemeSwitcher.sh
@@ -2,12 +2,13 @@
 
 # Define directories
 THEME_DIR="$HOME/.themes"
-GTK3_CONFIG_FILE="$HOME/.config/gtk-3.0/settings.ini"
 GTK2_CONFIG_FILE="$HOME/.gtkrc-2.0"
+GTK3_CONFIG_FILE="$HOME/.config/gtk-3.0/settings.ini"
+GTK4_CONFIG_DIR="$HOME/.config/gtk-4.0"
+GTK4_CONFIG_FILE="$HOME/.config/gtk-4.0/settings.ini"
 XSETTINGS_CONFIG_FILE="$HOME/.config/xsettingsd/xsettingsd.conf"
 BTOP_CONFIG_FILE="$HOME/.config/btop/btop.conf"
 CURRENT_THEME_LINK="$HOME/.themes/current"
-
 
 # Get theme options from the theme directory
 themes=()
@@ -26,20 +27,31 @@ if [ -z "$selected_theme" ]; then
 fi
 
 # --- Update GTK theme ---
+if [ -f "$GTK2_CONFIG_FILE" ]; then
+    sed -i "s|^gtk-theme-name=.*|gtk-theme-name=\"$selected_theme\"|" "$GTK2_CONFIG_FILE"
+fi
+
 if [ -f "$GTK3_CONFIG_FILE" ]; then
     sed -i "s|^gtk-theme-name=.*|gtk-theme-name=$selected_theme|" "$GTK3_CONFIG_FILE"
 fi
 
-if [ -f "$GTK2_CONFIG_FILE" ]; then
-    sed -i "s|^gtk-theme-name=.*|gtk-theme-name=\"$selected_theme\"|" "$GTK2_CONFIG_FILE"
+if [ -f "$GTK4_CONFIG_FILE" ]; then
+    sed -i "s|^gtk-theme-name=.*|gtk-theme-name=$selected_theme|" "$GTK4_CONFIG_FILE"
+fi
+
+if [ -d "$GTK4_CONFIG_DIR" ]; then
+    rm -f "$GTK4_CONFIG_DIR/assets" "$GTK4_CONFIG_DIR/gtk.css" "$GTK4_CONFIG_DIR/gtk-dark.css"
+    ln -sfn "$THEME_DIR/$selected_theme/gtk-4.0/assets" "$GTK4_CONFIG_DIR/assets"
+    ln -sfn "$THEME_DIR/$selected_theme/gtk-4.0/gtk.css" "$GTK4_CONFIG_DIR/gtk.css"
+    ln -sfn "$THEME_DIR/$selected_theme/gtk-4.0/gtk-dark.css" "$GTK4_CONFIG_DIR/gtk-dark.css"
 fi
 
 if [ -f "$XSETTINGS_CONFIG_FILE" ]; then
     sed -i "s|^Net/ThemeName.*|Net/ThemeName \"$selected_theme\"|" "$XSETTINGS_CONFIG_FILE"
 fi
 
-# Reload xsettingsd to apply the new theme
-pkill -HUP xsettingsd
+# Apply the theme using gsettings (the nwg-look way)
+gsettings set org.gnome.desktop.interface gtk-theme "$selected_theme"
 
 notify-send "Theme Switcher" "Switched GTK theme to $selected_theme."
 
@@ -56,7 +68,6 @@ if [ -f "$BTHEME_CONFIG_FILE" ]; then
     if [ -n "$btop_theme" ]; then
         # Update the color_theme line in btop config
         sed -i "s|^color_theme =.*|color_theme = \"$btop_theme\"|" "$BTOP_CONFIG_FILE"
-        pkill -SIGUSR2 btop
         notify-send "Theme Switcher" "Switched btop to $(basename "$btop_theme" .theme)."
     else
         notify-send "Theme Switcher" "No btop theme configured for $selected_theme."
@@ -94,5 +105,7 @@ reload_ghostty_windows() {
 
 # Reload ghostty to apply the new theme
 reload_ghostty_windows
+pkill -SIGUSR2 btop
+makoctl reload
 
 notify-send "Theme Switcher" "Switched to $selected_theme theme."
