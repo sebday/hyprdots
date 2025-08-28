fc63622b1a9b08a6ccb4d8d77e7a41ae8c26f9c2
Seb Day <sebday@gmail.com>
2025-08-28 23:27:49 +0100

    media as desktop apps

--- .local/bin/menu.sh
+++ .local/bin/menu.sh
@@ -2,7 +2,7 @@
 # A simple fuzzel-based menu script inspired by omarchy-menu.
 
 # Define menu options with Nerd Font icons
-options="󰸘 Change Theme\n󰋪 Change Wallpaper\n󰌌 Show Keybinds\n󰝚 Media Player\n󰟹 iPlayer\n󰗃 Channel 4"
+options="󰸘 Change Theme\n󰋪 Change Wallpaper\n󰌌 Show Keybinds\n󰝚 Media Player"
 
 # Use fuzzel to get the user's choice
 selected_option=$(echo -e "$options" | fuzzel -d -p "Select an action: ")
@@ -19,12 +19,6 @@ case "$selected_option" in
         ;;
     "󰝚 Media Player")
         bash ~/.local/bin/fuzzympv.sh
-        ;;
-    "󰟹 iPlayer")
-        brave --app="https://www.bbc.co.uk/iplayer/continue-watching"
-        ;;
-    "󰗃 Channel 4")
-        firefox --new-window https://www.channel4.com/my4/watching --class="Channel4"
         ;;
     *)
         # Exit gracefully if nothing was selected
