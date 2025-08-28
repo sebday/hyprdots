3cde51dfc3862e87f690f7508ac626ea7733e95b
Seb Day <sebday@gmail.com>
2025-08-28 21:23:12 +0100

    move media to system menu

--- .local/bin/menu.sh
+++ .local/bin/menu.sh
@@ -2,12 +2,11 @@
 # A simple fuzzel-based menu script inspired by omarchy-menu.
 
 # Define menu options with Nerd Font icons
-options="󰸘 Change Theme\n󰋪 Change Wallpaper\n󰌌 Show Keybinds"
+options="󰸘 Change Theme\n󰋪 Change Wallpaper\n󰌌 Show Keybinds\n󰝚 Media Player\n󰟹 iPlayer\n󰗃 Channel 4"
 
 # Use fuzzel to get the user's choice
 selected_option=$(echo -e "$options" | fuzzel -d -p "Select an action: ")
 
-# Execute the corresponding script based on the selection
 case "$selected_option" in
     "󰸘 Change Theme")
         bash ~/.local/bin/themes.sh
@@ -17,6 +16,15 @@ case "$selected_option" in
         ;;
     "󰌌 Show Keybinds")
         bash ~/.local/bin/keybinds.sh
+        ;;
+    "󰝚 Media Player")
+        bash ~/.local/bin/fuzzympv.sh
+        ;;
+    "󰟹 iPlayer")
+        brave --app="https://www.bbc.co.uk/iplayer/continue-watching"
+        ;;
+    "󰗃 Channel 4")
+        firefox --new-window https://www.channel4.com/my4/watching
         ;;
     *)
         # Exit gracefully if nothing was selected
