58623ba693299b10a4f312cd37f50a91aefc6eb4
Seb Day <sebday@gmail.com>
2025-09-27 19:27:08 +0100

    fix obsidian theme switch

--- .local/bin/themes-set-obsidian.sh
+++ .local/bin/themes-set-obsidian.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
 
-OBSIDIAN_THEME_DIR="$HOME/OneDrive/Notes/.obsidian/themes/Modular"
-OBSIDIAN_CONFIG_FILE="$HOME/OneDrive/Notes/.obsidian/appearance.json"
-OBSIDIAN_CSS_FILE="$HOME/.themes/current/obsidian.css"
-OBSIDIAN_SHARED_CSS_FILE="$HOME/.themes/shared/obsidian.css"
+# Update Obsidian theme
+OBSIDIAN_VAULT_DIR="$HOME/OneDrive/Notes"
+OBSIDIAN_CONFIG_FILE="$OBSIDIAN_VAULT_DIR/.obsidian/appearance.json"
+THEME_CSS_FILE="$HOME/.themes/current/obsidian.css"
 
-if [ -f "$OBSIDIAN_CSS_FILE" ]; then
+if [ -f "$THEME_CSS_FILE" ]; then
+    
+    MODULAR_THEME_DIR="$OBSIDIAN_VAULT_DIR/.obsidian/themes/Modular"
+    SHARED_CSS_FILE="$HOME/.themes/shared/obsidian.css"
 
-    # Ensure the modular theme directory exists
-    mkdir -p "$OBSIDIAN_THEME_DIR"
-
-    # Create a manifest.json if it doesn't exist by copying the shared one
-    SHARED_MANIFEST_FILE="$THEME_DIR/shared/obsidian.conf"
-    if [ ! -f "$OBSIDIAN_THEME_DIR/manifest.json" ] && [ -f "$SHARED_MANIFEST_FILE" ]; then
-        cp "$SHARED_MANIFEST_FILE" "$OBSIDIAN_THEME_DIR/manifest.json"
+    mkdir -p "$MODULAR_THEME_DIR"
+    
+    SHARED_MANIFEST_FILE="$HOME/.themes/shared/obsidian.conf"
+    if [ ! -f "$MODULAR_THEME_DIR/manifest.json" ] && [ -f "$SHARED_MANIFEST_FILE" ]; then
+        cp "$SHARED_MANIFEST_FILE" "$MODULAR_THEME_DIR/manifest.json"
+    fi
+    
+    if [ -f "$SHARED_CSS_FILE" ] && [ -f "$THEME_CSS_FILE" ]; then
+        cat "$THEME_CSS_FILE" "$SHARED_CSS_FILE" > "$MODULAR_THEME_DIR/theme.css"
     fi
 
-    # Combine theme-specific and shared CSS into the modular theme's css file
-    if [ -f "$OBSIDIAN_SHARED_CSS_FILE" ] && [ -f "$2" ]; then
-        cat "$OBSIDIAN_CSS_FILE" "$OBSIDIAN_SHARED_CSS_FILE" > "$OBSIDIAN_THEME_DIR/theme.css"
-    fi
-
-    # Ensure config file and directories exist
     OBSIDIAN_CONFIG_DIR=$(dirname "$OBSIDIAN_CONFIG_FILE")
     mkdir -p "$OBSIDIAN_CONFIG_DIR"
 
-    # Read existing config or create a default one
-    local updated_json
     if [ -f "$OBSIDIAN_CONFIG_FILE" ]; then
         updated_json=$(cat "$OBSIDIAN_CONFIG_FILE")
     else
         updated_json='{}'
     fi
 
-    # Set the theme to "Modular" and remove snippet settings
     updated_json=$(echo "$updated_json" | jq --arg theme "obsidian" '.theme = $theme' | jq --arg cssTheme "Modular" '.cssTheme = $cssTheme' | jq 'del(.enabledCssSnippets)')
 
-    # Write the updated json to the config file
     echo "$updated_json" > "$OBSIDIAN_CONFIG_FILE"
 fi
