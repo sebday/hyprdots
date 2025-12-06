eedf24834e3b7c124e77416f2042c0766c311b03
Seb Day <sebday@gmail.com>
2025-12-06 18:13:29 +0000

    update all themes

--- .local/bin/themes-set-mako.sh
+++ .local/bin/themes-set-mako.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Update mako theme
+# Update mako theme - just copy the theme file with include
 MAKO_THEME_FILE="$HOME/.themes/current/mako.ini"
 MAKO_CONFIG_FILE="$HOME/.config/mako/config"
 
 if [ -f "$MAKO_THEME_FILE" ]; then
-    # Copy the mako theme directly as it's in INI format
+    # Copy the mako theme file (which includes shared config)
     cp "$MAKO_THEME_FILE" "$MAKO_CONFIG_FILE"
 fi
