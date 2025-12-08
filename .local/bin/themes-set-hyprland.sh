2a315647e21c143680a43ec9fad82a76a208bf93
Seb Day <sebday@gmail.com>
2025-12-08 18:42:41 +0000

    simplify hypr theme swap

--- .local/bin/themes-set-hyprland.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update Hyprland theme colors
-HYPR_THEME_FILE="$HOME/.themes/current/hyprland.conf"
-HYPR_COLORS_FILE="$HOME/.config/hypr/theme-colors.conf"
-
-if [ -f "$HYPR_THEME_FILE" ]; then
-    cp "$HYPR_THEME_FILE" "$HYPR_COLORS_FILE"
-fi
