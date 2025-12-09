773730c4b42bb8f9254c1d90986890d18e03209b
Seb Day <sebday@gmail.com>
2025-12-09 01:09:22 +0000

    cache clean menu option

--- .local/bin/menu-main.sh
+++ .local/bin/menu-main.sh
@@ -1,5 +1,5 @@
 # Define menu options with Nerd Font icons
-options="󰸘 Change Theme\n󰋪 Change Wallpaper\n󰌌 Show Keybinds\n󰑐 Restart Waybar\n󰏔 Install Package"
+options="󰸘 Change Theme\n󰋪 Change Wallpaper\n󰌌 Show Keybinds\n󰑐 Restart Waybar\n󰏔 Install Package\n󰃨 Clear Cache"
 
 # Use fuzzel to get the user's choice
 selected_option=$(echo -e "$options" | fuzzel -d -p "Select an action: ")
@@ -20,6 +20,11 @@ case "$selected_option" in
         ;;
     "󰏔 Install Package")
         ghostty --class=TUI.float -e /home/seb/.local/bin/install-pkg.sh
+        ;;
+    "󰃨 Clear Cache")
+        rm -rf ~/.cache/thumbnails/* ~/.local/share/Trash/* ~/Projects/stable-diffusion-webui/{cache,outputs}/*
+        cliphist wipe
+        notify-send "Cache Cleared" "Thumbnails, Trash, Cache and clipboard history cleared"
         ;;
     *)
         # Exit gracefully if nothing was selected
