1fa5b3fef3ed2963055fdb2f5d3d7844bf8ccf1d
Seb Day <sebday@gmail.com>
2026-03-30 02:57:39 +0100

    new lumon & miasma themes

---
+++ .themes/lumon/neovim.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "omacom-io/lumon.nvim",
+    name = "lumon",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "lumon",
+    },
+  },
+}
