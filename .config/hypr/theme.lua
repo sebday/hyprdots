cad1c13fef22605a0b51edcac4cb4df250bb3ef9
Seb Day <sebday@gmail.com>
2026-08-06 02:41:21 +0100

    fix hyprctl dispatch with lua

--- .config/hypr/theme.lua
+++ .config/hypr/theme.lua
@@ -1,12 +1,12 @@
 hl.config({
     general = {
         col = {
-            active_border = "rgb(7daea3)",
+            active_border = "rgb(7fbbb3)",
         },
     },
     group = {
         col = {
-            border_active = "rgb(7daea3)",
+            border_active = "rgb(7fbbb3)",
         },
     },
 })
