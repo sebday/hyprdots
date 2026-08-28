98618c8919a221823a41b779125545ee4809d92c
Seb Day <sebday@gmail.com>
2026-08-28 20:55:07 +0100

    Neovim tweaks

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,4 +2,4 @@
 require("config.remote_clipboard").setup()
 
 vim.opt.relativenumber = false
-vim.g.autoformat = false
+vim.g.ai_cmp = false
--- .config/nvim/lua/plugins/completion-no-autoshow.lua
+++ .config/nvim/lua/plugins/completion-no-autoshow.lua
@@ -2,15 +2,27 @@ return {
 	"saghen/blink.cmp",
 	opts = {
 		completion = {
+			ghost_text = {
+				enabled = false,
+			},
 			menu = {
 				auto_show = false,
 			},
 			documentation = {
 				auto_show = false,
 			},
+			trigger = {
+				show_on_keyword = false,
+				show_on_trigger_character = false,
+				show_on_insert_on_trigger_character = false,
+				show_on_accept_on_trigger_character = false,
+			},
 		},
 		cmdline = {
 			completion = {
+				ghost_text = {
+					enabled = false,
+				},
 				menu = {
 					auto_show = false,
 				},
